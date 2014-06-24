fcfad8f259ffcad3f5ec3a3e5a55e54d448f6c5f
replica@mirror <team@replicalabs.com>
2014-06-24 18:17:39 -0400

    Fix commented out lines.

--- linux_setup.sh
+++ linux_setup.sh
@@ -8,11 +8,11 @@ CERES_PKGS="libgflags-dev libgoogle-glog-dev libatlas-base-dev libsuitesparse-de
 
 PACKAGES="ccache cmake cmake-curses-gui git build-essential subversion bzr default-jdk doxygen freeglut3 freeglut3-dev g++-4.8 gcc-4.8 git-core git-gui git-svn gitk graphviz htop libavcodec-dev libavformat-dev libavutil-dev libavahi-client-dev libavahi-compat-libdnssd-dev libblas3gf libblas-dev libboost1.54-all-dev libprotobuf-dev libprotobuf-c0 libprotobuf-c0-dev libprotobuf-lite8 libprotoc-dev protobuf-compiler libprotobuf8 libtbb-dev libtbb2 libuuid1 mercurial openssh-server openssh-client cppcheck glew-utils libglew-dev libxi-dev libxmu-dev libtool autoconf automake uuid-dev libuuid1 valgrind ant libsuitesparse-dev liblapack-dev libncurses5-dev nvidia-current-dev nvidia-cuda-toolkit nvidia-profiler nvidia-visual-profiler freeglut3 freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libgl1-mesa-glx libglu1-mesa libglu1-mesa-dev gcc g++ gcc-4.6 g++-4.6 linux-headers-generic linux-source libeigen3-dev libzmq3-dev cppcheck gnome-session-flashback gnome-tweak-tool emacs24 libpng12-dev $FFMPEG_PKGS $CERES_PKGS"
 
-#sudo apt-get install -y software-properties-common python-software-properties
-#sudo apt-get update -y
-#sudo apt-get dist-upgrade -y
-#sudo apt-get install -y -f $PACKAGES
-#sudo apt-get install --no-install-recommends -y ubuntu-desktop
+sudo apt-get install -y software-properties-common python-software-properties
+sudo apt-get update -y
+sudo apt-get dist-upgrade -y
+sudo apt-get install -y -f $PACKAGES
+sudo apt-get install --no-install-recommends -y ubuntu-desktop
 
 # Generate ssh key
 mkdir -p ~/.ssh
@@ -42,8 +42,8 @@ sudo chmod -R uga+rwx /usr/local
 
 mkdir -p $OPENCV_DIR/build
 cd $OPENCV_DIR/build
-#cmake .. -DCMAKE_BUILD_TYPE=Release
-#make install -j4
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make install -j4
 
 cd ~/setup
 
