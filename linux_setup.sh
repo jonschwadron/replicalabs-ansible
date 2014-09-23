7ada167ebfc0d5efda0a04728d949c2b1e5a4a68
Jack Morrison <jackmorrison1@gmail.com>
2014-09-23 13:33:56 -0600

    Shared libs for ceres.

--- linux_setup.sh
+++ linux_setup.sh
@@ -62,5 +62,5 @@ cd ceres-solver
 git checkout 1.8.0
 mkdir -p build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS:BOOL=ON
 make -j4 install
