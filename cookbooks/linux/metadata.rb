5d57e30be9fcd0880ae1eda6ddf20502d9730aef
jonschwadron <jonschwadron@gmail.com>
2014-11-25 09:41:58 +0000

    metadata.rb edited online with Bitbucket

--- cookbooks/linux/metadata.rb
+++ cookbooks/linux/metadata.rb
@@ -1,11 +1,6 @@
 name             'linux'
-<<<<<<< HEAD
 maintainer       'Replica Labs'
 maintainer_email 'jonschwadron@gmail.com'
-=======
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
->>>>>>> c0798e66067003378ba007b6b9a05c3ff371e71d
 license          'All rights reserved'
 description      'Installs/Configures linux'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
