571b6315a6382044c270210dd651c56d0d9c77b1
jonschwadron <jonschwadron@gmail.com>
2014-11-24 19:03:05 -0800

    added packages

--- cookbooks/linux/recipes/packages.rb
+++ cookbooks/linux/recipes/packages.rb
@@ -10,120 +10,156 @@
 
 SOURCE_DIRECTORY = File.join(Dir.home, "chef-repo/cookbooks/linux/files")
 
-FFMPEG_PKGS="
-libavcodec-dev
-libavdevice-dev
-libavfilter-dev
-libavformat-dev
-libavutil-dev
-libpostproc-dev
-libswscale-dev"
-
-CERES_PKGS="
-libatlas-base-dev
-libgflags-dev
-libgoogle-glog-dev
-libsuitesparse-dev"
-
-PYTHON_PKGS="
-pep8
-pyflakes
-python-pip"
-
-DB_PKGS="
-libmysqlclient-dev
-mysql-common"
-
-PACKAGES="
-$FFMPEG_PKGS
-$CERES_PKGS
-$PYTHON_PKGS
-$DB_PKGS
-ant
-autoconf
-automake
-build-essential
-bzr
-ccache
-checkstyle
-cmake
-cmake-curses-gui
-cppcheck
-cppcheck glew-utils
-default-jdk
-doxygen
-emacs24
-findbugs
-freeglut3
-freeglut3 freeglut3-dev
-freeglut3-dev
-g++
-g++-4.6
-g++-4.8
-gcc
-gcc-4.6
-gcc-4.8
-git
-git-core
-git-gui
-git-svn
-gitk
-gnome-session-flashback
-gnome-tweak-tool
-graphviz
-htop
-lcov
-libavahi-client-dev
-libavahi-compat-libdnssd-dev
-libblas-dev
-libblas3gf
-libboost1.54-all-dev
-libcppunit-dev
-libeigen3-dev
-libgl1-mesa-glx
-libglew-dev
-libglu1-mesa
-libglu1-mesa-dev
-liblapack-dev
-libncurses5-dev
-libpcl-all
-libpcl-bin
-libpng12-dev
-libprotobuf-c0
-libprotobuf-c0-dev
-libprotobuf-dev
-libprotobuf-lite8
-libprotobuf8
-libprotoc-dev
-libtbb-dev
-libtbb2
-libtool
-libuuid1
-libx11-dev
-libxi-dev
-libxmu-dev
-libzmq3-dev
-linux-headers-generic
-linux-image-generic
-linux-source
-mercurial
-meshlab
-nvidia-cuda-toolkit
-nvidia-current-dev
-nvidia-profiler
-nvidia-visual-profiler
-openssh-client
-openssh-server
-protobuf-compiler
-subversion
-uuid-dev
-valgrind
-xvfb"
-
+=begin
 file "#{SOURCE_DIRECTORY}/sources.list" do
-  #owner 'root'
-  #group 'root'
-  #mode 0755
+  owner 'root'
+  group 'root'
+  mode 0755
   content ::File.open("/etc/apt/sources.list").read
   action :create
 end
+=end
+
+execute "update package index" do
+  command "apt-get update"
+  ignore_failure true
+  action :nothing
+end.run_action(:run)
+
+#FFMPEGS PACKAGES
+%w{
+  libavcodec-dev
+  libavdevice-dev
+  libavfilter-dev
+  libavformat-dev
+  libavutil-dev
+  libpostproc-dev
+  libswscale-dev
+}.each do |pkg|
+  apt_package pkg do
+    action :install
+  end
+end
+
+#CERES PACKAGES
+%w{
+  libatlas-base-dev
+  libgflags-dev
+  libgoogle-glog-dev
+  libsuitesparse-dev
+}.each do |pkg|
+  apt_package pkg do
+    action :install
+  end
+end
+
+#PYTHON PACKAGES
+%w{
+  pep8
+  pyflakes
+  python-pip
+}.each do |pkg|
+  apt_package pkg do
+    action :install
+  end
+end
+
+#DB PACKAGES
+%w{
+  libmysqlclient-dev
+  mysql-common
+}.each do |pkg|
+  apt_package pkg do
+    action :install
+  end
+end
+
+#OTHER PACKAGES
+%w{
+  ant
+  autoconf
+  automake
+  build-essential
+  bzr
+  ccache
+  checkstyle
+  cmake
+  cmake-curses-gui
+  cppcheck
+  cppcheck glew-utils
+  default-jdk
+  doxygen
+  emacs24
+  findbugs
+  freeglut3
+  freeglut3 freeglut3-dev
+  freeglut3-dev
+  g++
+  g++-4.6
+  g++-4.8
+  gcc
+  gcc-4.6
+  gcc-4.8
+  git
+  git-core
+  git-gui
+  git-svn
+  gitk
+  gnome-session-flashback
+  gnome-tweak-tool
+  graphviz
+  htop
+  lcov
+  libavahi-client-dev
+  libavahi-compat-libdnssd-dev
+  libblas-dev
+  libblas3gf
+  libboost1.54-all-dev
+  libcppunit-dev
+  libeigen3-dev
+  libgl1-mesa-glx
+  libglew-dev
+  libglu1-mesa
+  libglu1-mesa-dev
+  liblapack-dev
+  libncurses5-dev
+  libpng12-dev
+  libprotobuf-c0
+  libprotobuf-c0-dev
+  libprotobuf-dev
+  libprotobuf-lite8
+  libprotobuf8
+  libprotoc-dev
+  libtbb-dev
+  libtbb2
+  libtool
+  libuuid1
+  libx11-dev
+  libxi-dev
+  libxmu-dev
+  libzmq3-dev
+  linux-headers-generic
+  linux-image-generic
+  linux-source
+  mercurial
+  meshlab
+  nvidia-cuda-toolkit
+  nvidia-current-dev
+  nvidia-profiler
+  nvidia-visual-profiler
+  openssh-client
+  openssh-server
+  protobuf-compiler
+  subversion
+  uuid-dev
+  valgrind
+  xvfb
+}.each do |pkg|
+  apt_package pkg do
+    action :install
+  end
+end
+
+#the following packages does not work:
+#libpcl-all
+#libpcl-bin
