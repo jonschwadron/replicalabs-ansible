873a61a5feec5aa2b6330bd25289da666ed10882
jonschwadron <jonschwadron@gmail.com>
2014-12-10 07:06:29 -0800

    fixed: indentations in repo recipe

--- cookbooks/replicalabs/recipes/repo.rb
+++ cookbooks/replicalabs/recipes/repo.rb
@@ -11,65 +11,65 @@
 SOURCE_DIRECTORY = File.join(Dir.home)
 
 %w{
-  miniglog
-  GLConsole
-  Sophus
-  Pangolin
-  Calibu
-  HAL
-  SceneGraph
-  Node
-  Kangaroo
-}.each do |repo|	
+	miniglog
+	GLConsole
+	Sophus
+	Pangolin
+	Calibu
+	HAL
+	SceneGraph
+	Node
+	Kangaroo
+	}.each do |repo|	
 
-  git "#{SOURCE_DIRECTORY}" do
-    repository "git@bitbucket.org:replicalabs/#{repo}"
-    revision "master"
-    action :sync
-  end
-  
-  directory "#{SOURCE_DIRECTORY}/#{repo}/build" do
-    owner 'root'
-    group 'root'
-    mode 0755
-  end
-
-  case repo
-	when "Sohpus"
-	  bash 'Install Sophus' do
-	   	cwd #{SOURCE_DIRECTORY}/#{repo}/build
-			code <<-EOH
-			  make .. -DCMAKE_INSTALL_PREFIX=$SRC_DIR/install -DBUILD_TESTS=OFF
-				make -j4 install
-			EOH
+		git "#{SOURCE_DIRECTORY}" do
+			repository "git@bitbucket.org:replicalabs/#{repo}"
+			revision "master"
+			action :sync
 		end
 
-	when "HAL"
-	  bash 'Install HAL' do
+		directory "#{SOURCE_DIRECTORY}/#{repo}/build" do
+			owner 'root'
+			group 'root'
+			mode 0755
+		end
+
+		case repo
+		when "Sohpus"
+			bash 'Install Sophus' do
+	   	cwd #{SOURCE_DIRECTORY}/#{repo}/build
+	   	code <<-EOH
+	   		make .. -DCMAKE_INSTALL_PREFIX=$SRC_DIR/install -DBUILD_TESTS=OFF
+	   		make -j4 install
+	   	EOH
+	   end
+
+	 when "HAL"
+	 	bash 'Install HAL' do
 	  	cwd #{SOURCE_DIRECTORY}/#{repo}/build
 	  	code <<-EOH
-		  	cmake .. -DCMAKE_INSTALL_PREFIX=$SRC_DIR/install -DBUILD_APPLICATIONS=OFF
-	   		make -j4 install
-			EOH
-		end
+	  		cmake .. -DCMAKE_INSTALL_PREFIX=$SRC_DIR/install -DBUILD_APPLICATIONS=OFF
+	  		make -j4 install
+	  	EOH
+	  end
 
 	when "Kangaroo"
-	  bash 'Install Kangaroo' do
+		bash 'Install Kangaroo' do
 	  	cwd #{SOURCE_DIRECTORY}/#{repo}/build
 	  	code <<-EOH
-		  	cmake .. -DCMAKE_INSTALL_PREFIX=$SRC_DIR/install -DBUILD_APPLICATIONS=OFF
-	   		make -j4 install
-		EOH
-		end
+	  		cmake .. -DCMAKE_INSTALL_PREFIX=$SRC_DIR/install -DBUILD_APPLICATIONS=OFF
+	  		make -j4 install
+	  	EOH
+	  end
 
 	else
-	  bash 'Install' do
+		bash 'Install' do
 	  	cwd #{SOURCE_DIRECTORY}/#{repo}/build
 	  	code <<-EOH
-		  	ccmake .. -DCMAKE_INSTALL_PREFIX=$SRC_DIR/install
-     		make -j4 install
-			EOH
-		end
+	  		ccmake .. -DCMAKE_INSTALL_PREFIX=$SRC_DIR/install
+	  		make -j4 install
+	  	EOH
+	  end
 	end
 end
 
@@ -81,11 +81,9 @@ end
 	meshify
 	}.each do |other_repo|	
 
-  git "#{SOURCE_DIRECTORY}" do
-    repository "git@bitbucket.org:replicalabs/#{other_repo}"
-    revision "master"
-    action :sync 
-  end
-end
-
-
+		git "#{SOURCE_DIRECTORY}" do
+			repository "git@bitbucket.org:replicalabs/#{other_repo}"
+			revision "master"
+			action :sync 
+		end
+	end
