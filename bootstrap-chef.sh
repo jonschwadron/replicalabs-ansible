26609555c86d7e8e7bbcfcb352728bc1cb18aa5a
jonschwadron <jonschwadron@gmail.com>
2015-01-06 15:41:36 -0800

    added bootstrap-chef.sh and removed old bash scripts

---
+++ bootstrap-chef.sh
@@ -0,0 +1,15 @@
+# Replica Labs Setup with Chef #
+
+Make sure your virtual machine is set to at least 2 GB (or higher) memory allocated, otherwise the setup will fail.  
+
+
+## Clone this repository into the chef-repo folder ##
+```
+git clone [https url] chef-repo
+```
+
+## Run setup.sh ##
+```
+cd chef-repo
+sudo bash setup.sh
+```
