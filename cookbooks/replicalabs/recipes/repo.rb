446ca55505603c539550787fb63d48eeb77bbb8d
jonschwadron <jonschwadron@gmail.com>
2015-01-09 10:31:08 -0800

    removed old builds and polished code

--- cookbooks/replicalabs/recipes/repo.rb
+++ cookbooks/replicalabs/recipes/repo.rb
@@ -8,89 +8,17 @@
 # Copyright 2014, Replica Labs
 # All rights reserved - Do Not Redistribute
 
-#require 'etc'
-#username = Etc.getlogin
-
-SOURCE_DIRECTORY = File.join(Dir.home)
-
 require 'etc'
 username = Etc.getlogin
 
-%w{
-  meshify-superbuild
-  rendor-android-superbuild
-  Server
-    }.each do |other_repo|    
+SOURCE_DIRECTORY = File.join(Dir.home)
+REPLICALABS_REPO = ['meshify-superbuild', 'rendor-android-superbuild']
 
-    git "#{SOURCE_DIRECTORY}/#{other_repo}" do
-      repository "git@bitbucket.org:replicalabs/#{other_repo}"
+(REPLICALABS_REPO).each do |repo|    
+    git "#{SOURCE_DIRECTORY}/#{repo}" do
+      repository "git@bitbucket.org:replicalabs/#{repo}"
       revision "master"
       user "#{username}"
       action :checkout
     end
 end
-
-=begin
-%w{
-  miniglog
-  GLConsole
-  SophusW
-  Pangolin
-  Calibu
-  HAL
-  SceneGraph
-  Node
-  Kangaroo
-  }.each do |repo|  
-
-    git "#{SOURCE_DIRECTORY}" do
-      repository "git@bitbucket.org:replicalabs/#{repo}"
-      revision "master"
-      action :sync
-    end
-
-    directory "#{SOURCE_DIRECTORY}/#{repo}/build" do
-      owner 'root'
-      group 'root'
-      mode 0755
-    end
-
-    case repo
-    when "Sohpus"
-      bash 'Install Sophus' do
-      cwd #{SOURCE_DIRECTORY}/#{repo}/build
-      code <<-EOH
-        make .. -DCMAKE_INSTALL_PREFIX=$SRC_DIR/install -DBUILD_TESTS=OFF
-        make -j4 install
-      EOH
-     end
-
-   when "HAL"
-    bash 'Install HAL' do
-      cwd #{SOURCE_DIRECTORY}/#{repo}/build
-      code <<-EOH
-        cmake .. -DCMAKE_INSTALL_PREFIX=$SRC_DIR/install -DBUILD_APPLICATIONS=OFF
-        make -j4 install
-      EOH
-    end
-
-  when "Kangaroo"
-    bash 'Install Kangaroo' do
-      cwd #{SOURCE_DIRECTORY}/#{repo}/build
-      code <<-EOH
-        cmake .. -DCMAKE_INSTALL_PREFIX=$SRC_DIR/install -DBUILD_APPLICATIONS=OFF
-        make -j4 install
-      EOH
-    end
-
-  else
-    bash 'Install' do
-      cwd #{SOURCE_DIRECTORY}/#{repo}/build
-      code <<-EOH
-        ccmake .. -DCMAKE_INSTALL_PREFIX=$SRC_DIR/install
-        make -j4 install
-      EOH
-    end
-  end
-end
-=end
